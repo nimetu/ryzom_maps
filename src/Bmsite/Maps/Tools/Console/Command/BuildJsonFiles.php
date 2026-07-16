a84856e35dcbb785e890c20d649ed041289f34a2
nimetu <nimetu@gmail.com>
2026-07-16 19:04:50 +0300

    Format code

--- src/Bmsite/Maps/Tools/Console/Command/BuildJsonFiles.php
+++ src/Bmsite/Maps/Tools/Console/Command/BuildJsonFiles.php
@@ -50,9 +50,9 @@ class BuildJsonFiles extends Command
      *
      * @throws \RuntimeException
      * @throws \InvalidArgumentException
-	 * @return int|null|void
-	 *
-	 * @mago-expect analysis:incompatible-return-type,missing-return-statement
+     * @return int|null|void
+     *
+     * @mago-expect analysis:incompatible-return-type,missing-return-statement
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
